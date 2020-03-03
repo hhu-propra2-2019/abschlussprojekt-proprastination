1451f8c6af02ad0c0166386b3d2193207a9c725f
Sotiris Galanos <sogal100@hhu.de>
2020-03-03 13:55:44 +0100

    changed package in mopsApplication to match actual folder

--- src/main/java/mops/MopsApplication.java
+++ src/main/java/mops/MopsApplication.java
@@ -1,4 +1,4 @@
-package mops.mops;
+package mops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MopsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MopsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MopsApplication.class, args);
+    }
 
 }
