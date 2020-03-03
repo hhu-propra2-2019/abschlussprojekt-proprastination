5297f794fd406586692ac53b57e92874c4985382
Sotiris Galanos <sogal100@hhu.de>
2020-03-03 14:45:57 +0100

    moved folders in test to prevent "mops.mops"

--- src/test/java/mops/MopsApplicationTests.java
+++ src/test/java/mops/MopsApplicationTests.java
@@ -1,4 +1,4 @@
-package mops.mops;
+package mops;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MopsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
