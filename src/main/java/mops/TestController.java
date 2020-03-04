a82707b6f7142b7a613cdbeba9530c61bb0205da
Julia Block <jublo@hhu.de>
2020-03-04 14:57:16 +0100

    TestController not working yet

---
+++ src/main/java/mops/TestController.java
@@ -0,0 +1,17 @@
+package mops;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bewerbung2")
+public class TestController {
+
+
+    @GetMapping("/test")
+    public String index(){
+        return "index";
+    }
+}
