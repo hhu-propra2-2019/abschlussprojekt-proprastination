0161057d8812e0fabb9d8b14401af57e7b1c2174
Sotiris Galanos <sogal100@hhu.de>
2020-03-05 13:57:49 +0100

    removed unused import

--- src/main/java/mops/organization/controllers/OrgaController.java
+++ src/main/java/mops/organization/controllers/OrgaController.java
@@ -7,7 +7,6 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
