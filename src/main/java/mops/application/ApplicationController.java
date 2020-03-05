99ab4fc7b19bf84b44a6d720f4f48dfaeb9e6c91
nemanjaZed <nemo.todorovic@gmail.com>
2020-03-05 14:27:19 +0100

    modified package structur and cleaned up the templates

--- src/main/java/mops/application/ApplicationController.java
+++ src/main/java/mops/application/ApplicationController.java
@@ -1,18 +1,16 @@
-package mops;
+package mops.application;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/bewerbung2")
-public class TestController {
+public class ApplicationController {
 
     @GetMapping("/")
-    public String bla(){
+    public String main(){
 
-        return "bla";
+        return "main";
     }
 }
