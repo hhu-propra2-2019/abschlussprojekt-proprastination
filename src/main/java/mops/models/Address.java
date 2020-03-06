7c9eaf64dcc42520c5fa4a3c672b9d9da259217d
iamkawaiineko <paulin.duerwald@gmail.com>
2020-03-06 15:32:07 +0100

    deleted models due to redundancy

--- src/main/java/mops/models/Address.java
+++
@@ -1,8 +0,0 @@
-package mops.models;
-
-public class Address {
-    private String street;
-    private String number;
-    private String postcode;
-    private String place;
-}
--- src/main/java/mops/models/Module.java
+++
@@ -1,9 +0,0 @@
-package mops.models;
-
-public class Module {
-    private String name;
-    private String grade;
-    private String semester;
-    private String task;
-    private String priority;
-}
