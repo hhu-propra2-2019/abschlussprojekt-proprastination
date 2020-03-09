4416245ee91ad636cc68065054f1e3eb7e44ff98
iamkawaiineko <paulin.duerwald@gmail.com>
2020-03-09 15:58:53 +0100

    added CSVService to services (service to read from .csv files)

---
+++ src/main/java/mops/services/CSVService.java
@@ -0,0 +1,38 @@
+package mops.services;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import org.springframework.stereotype.Service;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CSVService {
+
+    public static List<String[]> readFromCSV(String csvName){
+        List<String[]> help = new ArrayList<>();
+        try {
+            CSVReader csvReader = new CSVReader(new FileReader(csvName));
+            help = csvReader.readAll();
+            csvReader.close();
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+        return help;
+    }
+
+    public static List<String> getCountries(){
+        List<String> list = new ArrayList<>();
+        List<String[]> countries = new ArrayList<>();
+        countries = readFromCSV("csv/countries.csv");
+        String[] help;
+        for(int i = 0; i < countries.size(); i++){
+            help = countries.get(i);
+            list.add(help[0]);
+        }
+        return list;
+    }
+}
