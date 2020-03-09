686fd27c8ef2901ce9158d6a081fe6e012ae6737
Sotiris Galanos <sogal100@hhu.de>
2020-03-09 16:47:08 +0100

    added @SessionScope to Controllers

--- src/main/java/mops/controllers/DistributeController.java
+++ src/main/java/mops/controllers/DistributeController.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.Objects;
 
+@SessionScope
 @Controller
 @RequestMapping("/bewerbung2")
 public class DistributeController {
--- src/main/java/mops/controllers/OrgaController.java
+++ src/main/java/mops/controllers/OrgaController.java
@@ -8,8 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.annotation.SessionScope;
 
-import javax.servlet.ServletException;
+@SessionScope
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
