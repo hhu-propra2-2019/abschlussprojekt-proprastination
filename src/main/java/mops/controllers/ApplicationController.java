449f0468c655900866eda4e4bddcf910f338d4d2
Anloe109 <annika.loettgen@hhu.de>
2020-03-10 16:54:24 +0100

    changed folder structure again

--- src/main/java/mops/controllers/ApplicationController.java
+++ src/main/java/mops/controllers/ApplicationController.java
@@ -1,8 +1,5 @@
 package mops.controllers;
 
-import mops.organization.webclasses.Account;
-import org.keycloak.KeycloakPrincipal;
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
