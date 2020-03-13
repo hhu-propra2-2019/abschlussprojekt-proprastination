dd6fe20df0d2b835c8560649aff9d45969782c7c
ammon-s <ammon.sillah@hhu.de>
2020-03-13 16:28:05 +0100

    Adding dummy data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,172 @@
+INSERT INTO public.applicant (username, details)
+VALUES ('has220', '{
+  "name": "Batman",
+  "birthplace": "Gotham",
+  "address": {
+    "street": "Batstreet 1",
+    "city": "Gotham",
+    "country": "USA",
+    "zipcode": 42069
+  },
+  "birthday": "11.11.1111",
+  "nationality": "US of A**",
+  "course": "Hausbau",
+  "status": "NEW",
+  "certs": {
+    "name": "Basic Training",
+    "university": "Batcave"
+  },
+  "applications": [
+    {
+      "applicantusername": "has220",
+      "hours": 17,
+      "module": "Hausbau",
+      "priority": 2,
+      "grade": 1.3,
+      "lecturer": "Lala der Teletubby",
+      "semester": "SS2020",
+      "comment": "Boooob der Baumeister",
+      "role": "KORREKTOR"
+    },
+    {
+      "applicantusername": "has220",
+      "hours": 99,
+      "module": "Rächer",
+      "priority": 1,
+      "grade": 1.0,
+      "lecturer": "Ich selbst?",
+      "semester": "Immer",
+      "comment": "Ich bin Batman",
+      "role": "BOTH"
+    }
+  ]
+}');
+INSERT INTO public.applicant (username, details)
+VALUES ('basr221', '{
+  "name": "Pombaer",
+  "birthplace": "Lalaland",
+  "address": {
+    "street": "Lalastreet 122",
+    "city": "Tree",
+    "country": "Moon",
+    "zipcode": 42069
+  },
+  "birthday": "22.12.2111",
+  "nationality": "Treeborn",
+  "course": "Fliegen",
+  "status": "NEW",
+  "certs": {
+    "name": "Basic Training",
+    "university": "Imaginary"
+  },
+  "applications": [
+    {
+      "applicantusername": "basr221",
+      "hours": 17,
+      "module": "Hausbau",
+      "priority": 2,
+      "grade": 1.3,
+      "lecturer": "Lala der Teletubby",
+      "semester": "SS2020",
+      "comment": "Boooob der Baumeister",
+      "role": "KORREKTOR"
+    },
+    {
+      "applicantusername": "basr221",
+      "hours": 99,
+      "module": "Rächer",
+      "priority": 1,
+      "grade": 1.0,
+      "lecturer": "Ich selbst?",
+      "semester": "Immer",
+      "comment": "Ich bin Batman",
+      "role": "BOTH"
+    }
+  ]
+}');
+INSERT INTO public.applicant (username, details)
+VALUES ('bar221', '{
+  "name": "Batman",
+  "birthplace": "Gotham",
+  "address": {
+    "street": "Batstreet 1",
+    "city": "Gotham",
+    "country": "USA",
+    "zipcode": 42069
+  },
+  "birthday": "11.11.1111",
+  "nationality": "US of A**",
+  "course": "Hausbau",
+  "status": "NEW",
+  "certs": {
+    "name": "Basic Training",
+    "university": "Batcave"
+  },
+  "applications": [
+    {
+      "applicantusername": "bar221",
+      "hours": 17,
+      "module": "Hausbau",
+      "priority": 2,
+      "grade": 1.3,
+      "lecturer": "Lala der Teletubby",
+      "semester": "SS2020",
+      "comment": "Boooob der Baumeister",
+      "role": "KORREKTOR"
+    },
+    {
+      "applicantusername": "bar221",
+      "hours": 99,
+      "module": "Rächer",
+      "priority": 1,
+      "grade": 1.0,
+      "lecturer": "Ich selbst?",
+      "semester": "Immer",
+      "comment": "Ich bin Batman",
+      "role": "BOTH"
+    }
+  ]
+}');
+INSERT INTO public.applicant (username, details)
+VALUES ('blau222', '{
+  "name": "Batman",
+  "birthplace": "Gotham",
+  "address": {
+    "street": "Batstreet 1",
+    "city": "Gotham",
+    "country": "USA",
+    "zipcode": 42069
+  },
+  "birthday": "11.11.1111",
+  "nationality": "US of A**",
+  "course": "Hausbau",
+  "status": "NEW",
+  "certs": {
+    "name": "Basic Training",
+    "university": "Batcave"
+  },
+  "applications": [
+    {
+      "applicantusername": "blau222",
+      "hours": 17,
+      "module": "Hausbau",
+      "priority": 2,
+      "grade": 1.3,
+      "lecturer": "Lala der Teletubby",
+      "semester": "SS2020",
+      "comment": "Boooob der Baumeister",
+      "role": "KORREKTOR"
+    },
+    {
+      "applicantusername": "blau222",
+      "hours": 99,
+      "module": "Rächer",
+      "priority": 1,
+      "grade": 1.0,
+      "lecturer": "Ich selbst?",
+      "semester": "Immer",
+      "comment": "Ich bin Batman",
+      "role": "BOTH"
+    }
+  ]
+}');
