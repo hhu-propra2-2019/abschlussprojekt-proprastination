779c191cb0e3c87559e644086ab11cca3df68a28
Sotiris Galanos <sogal100@hhu.de>
2020-03-16 17:01:16 +0100

    Made Document Objects (with and without bachelor) and corrosponding interface

---
+++ src/main/java/mops/model/Document.java
@@ -0,0 +1,12 @@
+package mops.model;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface Document {
+    void save(File newFile) throws IOException;
+
+    void setField(String fieldName, String fieldContent) throws IOException;
+
+    void setGender(final String gender) throws IOException;
+}
---
+++ src/main/java/mops/model/DocumentWithBachelor.java
@@ -0,0 +1,43 @@
+package mops.model;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+
+import java.io.File;
+import java.io.IOException;
+
+public class DocumentWithBachelor implements Document {
+
+    private final String ORIGINAL_DOCUMENT = "323_Antrag_Beschaeftigung_wiss_Hilfskraefte_mit_BA.pdf";
+    private final PDDocument document;
+    private final PDAcroForm acroForm;
+
+    public DocumentWithBachelor() throws IOException {
+        document = PDDocument.load(new File(System.getProperty("user.dir") + File.separator + ORIGINAL_DOCUMENT));
+        document.setAllSecurityToBeRemoved(true);
+        acroForm = document.getDocumentCatalog().getAcroForm();
+    }
+
+    @Override
+    public void save(final File newFile) throws IOException {
+        document.save(newFile);
+    }
+
+    @Override
+    public void setField(final String fieldName, final String fieldContent) throws IOException {
+        acroForm.getField(fieldName).setValue(fieldContent);
+    }
+
+    /**
+     * Flips the given gender to comppensate a bug in the document
+     * @param gender the gender to be flipped
+     * @throws IOException ignored
+     */
+    public void setGender(final String gender) throws IOException {
+        if (gender.equals("männlich")) {
+            acroForm.getField("Geschlecht").setValue("weiblich");
+        } else if (gender.equals("weiblich")) {
+            acroForm.getField("Geschlecht").setValue("männlich");
+        }
+    }
+}
