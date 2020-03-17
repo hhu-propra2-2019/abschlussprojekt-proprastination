3a9c70c53a68ff38af9e2996622067ac7295d55e
Anloe109 <annika.loettgen@hhu.de>
2020-03-17 16:18:40 +0100

    added DistributorController

---
+++ src/main/java/mops/controllers/DistributorController.java
@@ -0,0 +1,51 @@
+package mops.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import mops.model.Account;
+import mops.services.ApplicantService;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.annotation.SessionScope;
+
+@SessionScope
+@Controller
+@RequestMapping("/bewerbung2/verteiler")
+public class DistributorController {
+
+    @Autowired
+    private ApplicantService applicantService;
+
+    private Account createAccountFromPrincipal(final KeycloakAuthenticationToken token) {
+        KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
+        return new Account(
+                principal.getName(),
+                principal.getKeycloakSecurityContext().getIdToken().getEmail(),
+                null,
+                token.getAccount().getRoles());
+    }
+
+    /**
+     * The GepMapping for the main page
+     *
+     * @param token The KeycloakAuthentication
+     * @param model The Website model
+     * @return The HTML file rendered as a String
+     */
+
+    @GetMapping("/")
+    @Secured("ROLE_verteiler")
+    public String index(final KeycloakAuthenticationToken token, final Model model) throws JsonProcessingException {
+        if (token != null) {
+            model.addAttribute("account", createAccountFromPrincipal(token));
+        }
+        return "distributor/distributorMain";
+    }
+}
