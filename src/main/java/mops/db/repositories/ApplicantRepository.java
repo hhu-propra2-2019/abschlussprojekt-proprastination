2b7fd60bbda39c52e1cb359d81725536bd4393fc
Sotiris Galanos <sogal100@hhu.de>
2020-03-17 21:09:46 +0100

    changed repositorys to make use of Hibernate

--- src/main/java/mops/db/repositories/ApplicantRepository.java
+++ src/main/java/mops/db/repositories/ApplicantRepository.java
@@ -1,33 +1,11 @@
 package mops.db.repositories;
 
-import mops.db.dto.ApplicantDTO;
-import org.springframework.data.jdbc.repository.query.Query;
+import mops.model.classes.Applicant;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 
 @Repository
-public interface ApplicantRepository extends CrudRepository<ApplicantDTO, Long> {
-
-    /**
-     * Search by Username.
-     *
-     * @param username username.
-     * @return JSON String.
-     */
-    @Query("SELECT * FROM applicant where uniserial = :username;")
-    ApplicantDTO findDistinctByUsername(@Param("username") String username);
-
-    /**
-     * Get id for username,
-     *
-     * @param username username.
-     * @return int id;
-     */
-    @Query("SELECT id FROM applicant where uniserial = :user")
-    Optional<Long> getIdByUsername(@Param("user") String username);
-
-
+public interface ApplicantRepository extends CrudRepository<Applicant, Long> {
+    Applicant findByName(String name);
 }
--- src/main/java/mops/db/repositories/ApplicationRepository.java
+++ src/main/java/mops/db/repositories/ApplicationRepository.java
@@ -1,21 +1,9 @@
 package mops.db.repositories;
 
-import mops.db.dto.ApplicationDTO;
-import org.springframework.data.jdbc.repository.query.Query;
+import mops.model.classes.Application;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
-public interface ApplicationRepository extends CrudRepository<ApplicationDTO, Long> {
-
-    /**
-     * Returns all ApplicationDTOs for a given module.
-     *
-     * @param module
-     * @return List<ApplicationDTO>
-     */
-    @Query("SELECT * from application where module = :module")
-    List<ApplicationDTO> findAllByModule(@Param("module") String module);
+public interface ApplicationRepository extends CrudRepository<Application, Long> {
 
 }
