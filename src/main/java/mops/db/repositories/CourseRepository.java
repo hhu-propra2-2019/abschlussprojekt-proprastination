55929db04627423fb81df4e7a22cbbd3ad29c230
Sotiris Galanos <sogal100@hhu.de>
2020-03-17 21:10:08 +0100

    added new repositorys for courses and modules

---
+++ src/main/java/mops/db/repositories/CourseRepository.java
@@ -0,0 +1,13 @@
+package mops.db.repositories;
+
+import mops.model.classes.Course;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CourseRepository extends CrudRepository<Course, Long> {
+    @Override
+    List<Course> findAll();
+}
