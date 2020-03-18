396a0423f7cf4d72450e76d20e1534bf28a266c1
ammon-s <ammon.sillah@hhu.de>
2020-03-18 15:56:00 +0100

    Comments and small changes

--- src/main/java/mops/services/PDFService.java
+++ src/main/java/mops/services/PDFService.java
@@ -7,28 +7,32 @@ import mops.model.classes.Applicant;
 import mops.model.classes.Application;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 @Service
 public class PDFService {
 
     private Document document;
 
-    @PostConstruct
-    public void test() throws IOException {
-        document = new DocumentWithBachelor();
-        document.debug();
-        document.addGeneralInfos();
-        document.save(new File("/home/sotga/Schreibtisch/test.pdf"));
-    }
-
-    public void generatePDF(final Application application, final Applicant applicant) throws IOException {
+    /**
+     * Generates an Application PDF given the parameters.
+     *
+     * @param application Application.
+     * @param applicant   Applicant.
+     * @return filepath to file.
+     * @throws IOException If Document could not be generated.
+     */
+    public String generatePDF(final Application application, final Applicant applicant) throws IOException {
+        UUID uuid = UUID.randomUUID();
+        String filepath = "/tmp/pdf/" + uuid.toString() + ".pdf";
         document = new DocumentWithBachelor();
         addApplicationInfoToPDF(application);
         addApplicantInfoToPDF(applicant);
         document.addGeneralInfos();
+        document.save(new File(filepath));
+        return filepath;
     }
 
     private void addApplicationInfoToPDF(final Application application) throws IOException {
