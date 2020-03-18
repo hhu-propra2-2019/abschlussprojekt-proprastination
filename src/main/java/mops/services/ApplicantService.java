e6122365edc37aa5f140ab0b6d112ea8fac6097d
Sotiris Galanos <sogal100@hhu.de>
2020-03-18 14:31:41 +0100

    added applicant service

--- src/main/java/mops/services/ApplicantService.java
+++ src/main/java/mops/services/ApplicantService.java
@@ -1,9 +1,11 @@
 package mops.services;
 
-import mops.db.repositories.ApplicantRepository;
 import mops.model.classes.Applicant;
+import mops.repositories.ApplicantRepository;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @EnableAutoConfiguration
@@ -16,6 +18,18 @@ public class ApplicantService {
     }
 
     public Applicant findByUsername(final String username) {
-        return applicantRepository.findByName(username);
+        return applicantRepository.findByFirstName(username);
+    }
+
+    public void saveApplicant(final Applicant applicant) {
+        applicantRepository.save(applicant);
+    }
+
+    public List<Applicant> findAll() {
+        return applicantRepository.findAll();
+    }
+
+    public Applicant findByUniserial(final String uniserial) {
+        return applicantRepository.findByUniserial(uniserial).get(0);
     }
 }
