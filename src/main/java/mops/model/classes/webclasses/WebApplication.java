4b7ab83befbf8db02e93744949ac6e1d28eb2662
iamkawaiineko <paulin.duerwald@gmail.com>
2020-03-19 16:43:09 +0100

    removed stray > from applicationPersonal.html and added object wrappings to applicationModuleThymeleaf.html

--- src/main/java/mops/model/classes/webclasses/WebApplication.java
+++ src/main/java/mops/model/classes/webclasses/WebApplication.java
@@ -1,9 +1,6 @@
 package mops.model.classes.webclasses;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,4 +8,12 @@ import lombok.NoArgsConstructor;
 @Builder(toBuilder = true)
 public class WebApplication {
     private String module;
+    private int workload;
+    private int priority;
+    private double grade;
+    private String lecturer;
+    private String semester;
+    private String role;
+    private String comment;
+
 }
