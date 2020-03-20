6ec48ed5810cb5b04919cc7980ebd71acc0c6301
Anloe109 <annika.loettgen@hhu.de>
2020-03-20 16:32:44 +0100

    maybe working algorithm now, cant check

--- src/main/java/mops/services/DistributionService.java
+++ src/main/java/mops/services/DistributionService.java
@@ -1,10 +1,16 @@
 package mops.services;
 
+import mops.model.classes.Applicant;
+import mops.model.classes.Application;
 import mops.model.classes.Distribution;
+import mops.model.classes.Evaluation;
 import mops.repositories.DistributionRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class DistributionService {
@@ -12,20 +18,28 @@ public class DistributionService {
     private final DistributionRepository distributionRepository;
     private final ModuleService moduleService;
     private final ApplicantService applicantService;
+    private final ApplicationService applicationService;
+    private final EvaluationService evaluationService;
 
     /**
      * Injects Services and repositories
      * @param distributionRepository the injected repository
      * @param moduleService the services that manages modules
      * @param applicantService the services that manages applicants
+     * @param applicationService the services that manages applications
+     * @param evaluationService the services that manages evaluations
      */
     @SuppressWarnings("checkstyle:HiddenField")
     public DistributionService(final DistributionRepository distributionRepository,
                                final ModuleService moduleService,
-                               final ApplicantService applicantService) {
+                               final ApplicantService applicantService,
+                               final ApplicationService applicationService,
+                               final EvaluationService evaluationService) {
         this.distributionRepository = distributionRepository;
         this.moduleService = moduleService;
         this.applicantService = applicantService;
+        this.applicationService = applicationService;
+        this.evaluationService = evaluationService;
         assign();
     }
 
@@ -37,6 +51,76 @@ public class DistributionService {
                 .employees(applicantService.findAll())
                 .module("unassigned")
                 .build());
+    }
+
+    /**
+     * automatically distributes applicants for one module
+     * @param module
+     * @return List of assigned applicants
+     */
+    private List<Applicant> distribute(final Module module) {
+        List<Applicant> result = new LinkedList<>();
+        List<Application> applications = applicationService.findApplicationByModuleAndHours(module.getName(), 7);
+        Map<Application, Integer> sortedApplications = new LinkedHashMap<>();
+        int count = 0;
+
+        for (Application application : applications) {
+            int sum = application.getPriority() + evaluationService.findByApplication(application).getPriority();
+            sortedApplications.put(application, sum);
+        }
+        sortedApplications.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEachOrdered(x -> sortedApplications.put(x.getKey(), x.getValue()));
+        for (Application application : applications) {
+            if (count >= module.getMax7()) {
+                break;
+            }
+            result.add(application.getApplicant());
+            count++;
+        }
+
+        applications = applicationService.findApplicationByModuleAndHours(module.getName(), 9);
+        sortedApplications = new LinkedHashMap<>();
+        count = 0;
+
+        for (Application application : applications) {
+            int sum = application.getPriority() + evaluationService.findByApplication(application).getPriority();
+            sortedApplications.put(application, sum);
+        }
+        sortedApplications.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEachOrdered(x -> sortedApplications.put(x.getKey(), x.getValue()));
+        for (Application application : applications) {
+            if (count >= module.getMax7()) {
+                break;
+            }
+            result.add(application.getApplicant());
+            count++;
+        }
+
+        applications = applicationService.findApplicationByModuleAndHours(module.getName(), 17);
+        sortedApplications = new LinkedHashMap<>();
+        count = 0;
+
+        for (Application application : applications) {
+            int sum = application.getPriority() + evaluationService.findByApplication(application).getPriority();
+            sortedApplications.put(application, sum);
+        }
+        sortedApplications.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEachOrdered(x -> sortedApplications.put(x.getKey(), x.getValue()));
+        for (Application application : applications) {
+            if (count >= module.getMax7()) {
+                break;
+            }
+            result.add(application.getApplicant());
+            count++;
+        }
+
+        return result;
     }
 
     /**
