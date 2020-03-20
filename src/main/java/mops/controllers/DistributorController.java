83a23cfc3b953848b9e63ce6db724f8c41aa1e07
Marcel Gerken <marcelgerken4900@gmail.com>
2020-03-20 14:41:11 +0100

    First version of GetMapping for Distributor

--- src/main/java/mops/controllers/DistributorController.java
+++ src/main/java/mops/controllers/DistributorController.java
@@ -2,7 +2,14 @@ package mops.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import mops.model.Account;
+import mops.model.classes.Applicant;
+import mops.model.classes.Application;
+import mops.model.classes.Distribution;
+import mops.model.classes.Evaluation;
+import mops.model.classes.webclasses.WebDistribution;
+import mops.model.classes.webclasses.WebDistributorApplicant;
 import mops.services.ApplicantService;
+import mops.services.DistributionService;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +20,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.annotation.SessionScope;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SessionScope
 @Controller
 @RequestMapping("/bewerbung2/verteiler")
@@ -20,6 +30,7 @@ public class DistributorController {
 
     @Autowired
     private ApplicantService applicantService;
+    private DistributionService distributionService;
 
     private Account createAccountFromPrincipal(final KeycloakAuthenticationToken token) {
         KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
@@ -38,11 +49,59 @@ public class DistributorController {
      * @return The HTML file rendered as a String
      */
 
+    @SuppressWarnings("checkstyle:MagicNumber")
     @GetMapping("/")
     @Secured("ROLE_verteiler")
     public String index(final KeycloakAuthenticationToken token, final Model model) throws JsonProcessingException {
         if (token != null) {
             model.addAttribute("account", createAccountFromPrincipal(token));
+            List<WebDistribution> webDistributionList = new ArrayList<>();
+            List<Distribution> distributionList = new ArrayList<>();
+            for (Distribution distribution : distributionList) {
+                List<WebDistributorApplicant> webDistributorApplicantList = new ArrayList<>();
+                List<Applicant> applicantList = new ArrayList<>();
+                //TODO Applicant wieder austauschen
+                for (Applicant applicant : applicantList) {
+                    Application application = Application.builder()
+                            .applicant(applicant)
+                            .comment("ich bin sehr gut")
+                            .grade(1.0)
+                            .hours(17)
+                            .id(1)
+                            .lecturer("prof")
+                            .module(distribution.getModule())
+                            .priority(2)
+                            .role("Korrektor")
+                            .semester("SS19")
+                            .build();
+                    //Application application = applicantService.findApplicationByUsernameAndModule(
+                    //        applicantList.get(j).getUniserial(),
+                    //        distributionList.get(i).getModule());
+                    //Evaluation evaluation = evaluationService.findByApplication(application);
+                    //TODO folgende Evaluation wieder rausnehmen
+                    Evaluation evaluation = Evaluation.builder()
+                            .application(application)
+                            .comment("blablabla")
+                            .id(1)
+                            .priority(1)
+                            .build();
+                    WebDistributorApplicant webDistributorApplicant = WebDistributorApplicant.builder()
+                            .username(applicant.getUniserial())
+                            .applicantPriority(application.getPriority() + "")
+                            .minHours(application.getHours() + "")
+                            .maxHours(application.getHours() + "")
+                            .organizerPriority(evaluation.getPriority() + "")
+                            //.organizerHours(evaluation.getHours + "")
+                            .build();
+                    webDistributorApplicantList.add(webDistributorApplicant);
+                }
+                WebDistribution webDistribution = WebDistribution.builder()
+                        .module(distribution.getModule())
+                        .webDistributorApplicants(webDistributorApplicantList)
+                        .build();
+                webDistributionList.add(webDistribution);
+            }
+            model.addAttribute(webDistributionList);
         }
         return "distributor/distributorMain";
     }
---
+++ src/main/java/mops/model/classes/webclasses/WebDistributorApplicant.java
@@ -0,0 +1,20 @@
+package mops.model.classes.webclasses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder(toBuilder = true)
+public class WebDistributorApplicant {
+
+    private String username;
+    private String applicantPriority;
+    private String organizerPriority;
+    private String minHours;
+    private String maxHours;
+    private String organizerHours;
+}
