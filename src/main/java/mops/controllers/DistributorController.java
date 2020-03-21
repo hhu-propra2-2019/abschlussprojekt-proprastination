000522daf1105845ba666f2238af31acbbdd603f
Marcel Gerken <marcelgerken4900@gmail.com>
2020-03-21 13:23:28 +0100

    removed dead Code

--- src/main/java/mops/controllers/DistributorController.java
+++ src/main/java/mops/controllers/DistributorController.java
@@ -2,16 +2,8 @@ package mops.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import mops.model.Account;
-import mops.model.classes.Applicant;
-import mops.model.classes.Application;
-import mops.model.classes.Distribution;
-import mops.model.classes.Evaluation;
 import mops.model.classes.webclasses.WebDistribution;
-import mops.model.classes.webclasses.WebDistributorApplicant;
-import mops.model.classes.webclasses.WebDistributorApplication;
-import mops.services.ApplicationService;
 import mops.services.DistributionService;
-import mops.services.EvaluationService;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.security.access.annotation.Secured;
@@ -21,9 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.annotation.SessionScope;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+
 
 @SessionScope
 @Controller
@@ -31,23 +22,14 @@ import java.util.Set;
 public class DistributorController {
 
     private final DistributionService distributionService;
-    private final ApplicationService applicationService;
-    private final EvaluationService evaluationService;
-
 
     /**
      * Constructor
      * @param distributionService
-     * @param applicationService
-     * @param evaluationService
      */
     @SuppressWarnings("checkstyle:HiddenField")
-    public DistributorController(final DistributionService distributionService,
-                                 final ApplicationService applicationService,
-                                 final EvaluationService evaluationService) {
+    public DistributorController(final DistributionService distributionService) {
         this.distributionService = distributionService;
-        this.applicationService = applicationService;
-        this.evaluationService = evaluationService;
     }
 
     private Account createAccountFromPrincipal(final KeycloakAuthenticationToken token) {
