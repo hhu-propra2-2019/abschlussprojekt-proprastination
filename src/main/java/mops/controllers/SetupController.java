520c18f35c6975c99de365efe10240e56586d064
Julia Block <jublo@hhu.de>
2020-03-24 13:26:24 +0100

    Added methods to webmoduleservice and repository, changed controller to work with db

--- src/main/java/mops/controllers/SetupController.java
+++ src/main/java/mops/controllers/SetupController.java
@@ -3,9 +3,10 @@ package mops.controllers;
 import mops.model.Account;
 import mops.model.classes.Module;
 import mops.model.classes.webclasses.WebModule;
-import mops.services.CSVService;
+import mops.services.WebModuleService;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,13 +16,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.annotation.SessionScope;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @SessionScope
 @RequestMapping("/bewerbung2/setup")
 public class SetupController {
+    @Autowired
+    private WebModuleService webService;
 
     private Account createAccountFromPrincipal(final KeycloakAuthenticationToken token) {
         KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
@@ -42,7 +44,7 @@ public class SetupController {
     @Secured("ROLE_setup")
     public String index(final KeycloakAuthenticationToken token, final Model model) {
         if (token != null) {
-            List<WebModule> modules = CSVService.getModulesWithDetails();
+            List<WebModule> modules = webService.getModules();
             model.addAttribute("modules", modules);
             model.addAttribute("account", createAccountFromPrincipal(token));
             model.addAttribute("module", Module.builder().build());
@@ -63,11 +65,14 @@ public class SetupController {
     public String postEditedModule(final KeycloakAuthenticationToken token, final Model model,
                                    @RequestParam("oldName") final String oldName,
                                    final WebModule module) {
-        CSVService.deleteModule(oldName);
+        //webService.deleteOne(oldName);
+        //webService.update(module);
+
+        /*CSVService.deleteModule(oldName);
         List<String[]> input = new ArrayList<>();
         String[] editedModule = module.toStringArray();
         input.add(editedModule);
-        CSVService.writeInCSV("src/main/resources/csv/module.csv", input);
+        CSVService.writeInCSV("src/main/resources/csv/module.csv", input);*/
         return index(token, model);
     }
 
@@ -82,7 +87,7 @@ public class SetupController {
     public String newModule(final KeycloakAuthenticationToken token, final Model model) {
         if (token != null) {
             model.addAttribute("account", createAccountFromPrincipal(token));
-            model.addAttribute("Module", Module.builder().build());
+            model.addAttribute("Module", WebModule.builder().build());
         }
         return "setup/neuesModul";
     }
@@ -98,10 +103,7 @@ public class SetupController {
     @Secured("ROLE_setup")
     public String postNewModule(final KeycloakAuthenticationToken token, final Model model,
                                 final WebModule module) {
-        List<String[]> input = new ArrayList<>();
-        String[] newModule = module.toStringArray();
-        input.add(newModule);
-        CSVService.writeInCSV("src/main/resources/csv/module.csv", input);
+        webService.save(module);
         return index(token, model);
     }
 
@@ -132,7 +134,7 @@ public class SetupController {
     @Secured("ROLE_setup")
     public String postDeleteModule(final KeycloakAuthenticationToken token, final Model model,
                                 @RequestParam("nameDelete") final String name) {
-        CSVService.deleteModule(name);
+        //webService.deleteOne(name);
         return index(token, model);
     }
 
@@ -145,7 +147,7 @@ public class SetupController {
     @PostMapping("/alleModuleLoeschen")
     @Secured("ROLE_setup")
     public String postDeleteAllModule(final KeycloakAuthenticationToken token, final Model model) {
-        CSVService.cleanModules();
+        webService.deleteAll();
         return index(token, model);
     }
 }
