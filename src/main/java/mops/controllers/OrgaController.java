f5542f34b5066aa0fc7870fa907b8ff6542aaa92
ammon-s <ammon.sillah@hhu.de>
2020-03-25 11:48:06 +0100

    Changed Controller and OverViewHTMl to accommodate wrapper class

--- src/main/java/mops/controllers/OrgaController.java
+++ src/main/java/mops/controllers/OrgaController.java
@@ -1,15 +1,12 @@
 package mops.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import mops.model.Account;
-import mops.model.classes.orgaWebClasses.OrgaApplication;
-import mops.services.ApplicantService;
-import mops.services.ApplicationService;
+import mops.model.classes.orgaWebClasses.WebList;
+import mops.model.classes.orgaWebClasses.WebListClass;
 import mops.services.ModuleService;
 import mops.services.OrgaService;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
-import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,10 +15,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.annotation.SessionScope;
 
-import java.util.LinkedList;
 import java.util.List;
 
 @SessionScope
@@ -29,23 +24,17 @@ import java.util.List;
 @RequestMapping("/bewerbung2/organisator")
 public class OrgaController {
 
-    private final ApplicantService applicantService;
-    private final ApplicationService applicationService;
     private final ModuleService moduleService;
     private final OrgaService orgaService;
 
     /**
      * Lets Spring inject the services
-     * @param applicantService
-     * @param applicationService
-     * @param moduleService
-     * @param orgaService
+     *
+     * @param moduleService moduleService
+     * @param orgaService   orgaService
      */
     @SuppressWarnings("checkstyle:HiddenField")
-    public OrgaController(final ApplicantService applicantService, final ApplicationService applicationService,
-                          final ModuleService moduleService, final OrgaService orgaService) {
-        this.applicantService = applicantService;
-        this.applicationService = applicationService;
+    public OrgaController(final ModuleService moduleService, final OrgaService orgaService) {
         this.moduleService = moduleService;
         this.orgaService = orgaService;
     }
@@ -69,7 +58,7 @@ public class OrgaController {
 
     @GetMapping("/")
     @Secured("ROLE_orga")
-    public String index(final KeycloakAuthenticationToken token, final Model model) throws JsonProcessingException {
+    public String index(final KeycloakAuthenticationToken token, final Model model) {
         if (token != null) {
             model.addAttribute("account", createAccountFromPrincipal(token));
         }
@@ -78,33 +67,11 @@ public class OrgaController {
     }
 
     /**
-     * PostMapping to save changes
-     * @param token
-     * @param model
-     * @param priority
-     * @param hours
-     * @param comment
-     * @return orgaMain.html rendered as a String
-     * @throws JsonProcessingException
-     */
-    @PostMapping("/")
-    //@Secured("ROLE orga")
-    public String save(final KeycloakAuthenticationToken token, final Model model,
-                       @RequestParam("priority") final String priority,
-                       @RequestParam("hours") final String hours,
-                       @RequestParam("comment") final String comment) throws JsonProcessingException {
-        if (token != null) {
-            model.addAttribute("account", createAccountFromPrincipal(token));
-        }
-        return "organizer/orgaMain";
-    }
-
-
-    /**
      * Shows overview of applications for a module.
-     * @param id the applications is, as Path variable
-     * @param token
-     * @param model
+     *
+     * @param id    the applications is, as Path variable
+     * @param token Keycloak token
+     * @param model Model.
      * @return orgaOverview.html as String
      */
     @GetMapping("/{id}/")
@@ -114,25 +81,23 @@ public class OrgaController {
         if (token != null) {
             model.addAttribute("account", createAccountFromPrincipal(token));
         }
-        model.addAttribute("applications", orgaService.getAllApplications(id));
+        List<WebList> applications = orgaService.getAllListEntrys(id);
+        WebListClass webListClass = new WebListClass(applications);
+        model.addAttribute("WebList", webListClass);
         return "organizer/orgaOverview";
     }
 
     /**
-     *
-     * @param applications
-     * @param id
-     * @param model
-     * @return
+     * @param applications WebListClass applications
+     * @param id           module id
+     * @param model        model
+     * @return redirect orgaOverview
      */
     @PostMapping("/{id}/")
     @Secured("ROLE_orga")
-    public String applicationInfoPost(final LinkedList<OrgaApplication> applications,
+    public String applicationInfoPost(@ModelAttribute final WebListClass applications,
                                       @PathVariable("id") final String id, final Model model) {
-        System.out.println("---------------------------------------------------------------------");
-        System.out.println(applications.size());
-        applications.forEach(System.out::println);
-        System.out.println("---------------------------------------------------------------------");
+        orgaService.saveEvaluations(applications);
         return "redirect:/bewerbung2/organisator/" + id + "/";
     }
 
@@ -140,7 +105,7 @@ public class OrgaController {
      * Needed to display additional information about each application on the overview page.
      * (Inside a modal / popup window.)
      * @param id applications id
-     * @param model
+     * @param model Model
      * @return "applicationModalContent", the HTML file with the modal content.
      */
     @GetMapping("/modal/{id}/")
