8a355edcef270ab933e176be7344e76aaf72ef32
chber119 <chiara.berry@gmx.de>
2020-03-26 21:05:11 +0100

    test equals in Module

--- src/test/java/mops/model/classes/ModuleTest.java
+++ src/test/java/mops/model/classes/ModuleTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.time.Instant;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class ModuleTest {
 
@@ -38,6 +40,40 @@ class ModuleTest {
                 .hasFieldOrPropertyWithValue("nineHourLimit", "9")
                 .hasFieldOrPropertyWithValue("seventeenHourLimit", "17")
                 .hasFieldOrPropertyWithValue("hourLimit", "42");
+    }
+
+    @Test
+    void testEquals() {
+        Module newModule = Module.builder()
+                .name("Introduction to Logic")
+                .deadline(Instant.parse("2018-11-30T18:35:24.00Z"))
+                .shortName("Logic")
+                .profName("Hershel Layton")
+                .sevenHourLimit("7")
+                .nineHourLimit("9")
+                .seventeenHourLimit("17")
+                .hourLimit("42")
+                .build();
+
+        assertEquals(module, newModule);
+        assertEquals(newModule, module);
+    }
+
+    @Test
+    void testEqualsNotEqual() {
+        Module newModule = Module.builder()
+                .name("Introduction to Logic")
+                .deadline(Instant.parse("2018-11-30T18:35:24.00Z"))
+                .shortName("Logic")
+                .profName("Hershel Layton")
+                .sevenHourLimit("7")
+                .nineHourLimit("9")
+                .seventeenHourLimit("17")
+                .hourLimit("777")
+                .build();
+
+        assertNotEquals(module, newModule);
+        assertNotEquals(newModule, module);
     }
 
 }
