107e3b3f7fa8a21c0d454e201da9b5e688d7bb0d
chber119 <chiara.berry@gmx.de>
2020-03-27 18:17:45 +0100

    add more testcases

--- src/test/java/mops/services/dbServices/ApplicantServiceTest.java
+++ src/test/java/mops/services/dbServices/ApplicantServiceTest.java
@@ -142,4 +142,63 @@ class ApplicantServiceTest {
         assertThat(resultApplicant.getApplications()).contains(application2);
     }
 
+    @Test
+    void deleteSecondApplication() {
+        moduleRepository.save(module);
+        applicantService.saveApplicant(applicant);
+
+        applicantService.deleteApplication(application2, applicant);
+
+        Applicant resultApplicant = applicantService.findByUniserial(applicant.getUniserial());
+        assertThat(resultApplicant.getApplications()).contains(application1);
+        assertThat(resultApplicant.getApplications()).doesNotContain(application2);
+    }
+
+    @Test
+    void deleteNotFoundApplication() {
+        moduleRepository.save(module);
+        applicantService.saveApplicant(applicant);
+        Application application3 = Application.builder()
+                .priority(Priority.NEUTRAL)
+                .module(module)
+                .minHours(9)
+                .maxHours(9)
+                .grade(3.0)
+                .lecturer("Charles Xavier")
+                .role(Role.BOTH)
+                .semester("SS2019")
+                .build();
+
+        applicantService.deleteApplication(application3, applicant);
+
+        Applicant resultApplicant = applicantService.findByUniserial(applicant.getUniserial());
+        assertThat(resultApplicant.getApplications()).contains(application1);
+        assertThat(resultApplicant.getApplications()).contains(application2);
+        assertThat(resultApplicant.getApplications()).doesNotContain(application3);
+    }
+
+    @Test
+    void testDeleteApplicationSetOfApplicationsIsEmpty() {
+        Set<Application> applications = new HashSet<>();
+        Applicant testApplicant = Applicant.builder()
+                .applications(applications)
+                .status("New")
+                .surname("dumm")
+                .uniserial("sss111")
+                .course("Hausbau")
+                .comment("WOW!")
+                .birthday("11.11.1111")
+                .address(address)
+                .nationality("US of A**")
+                .birthplace("Gotham")
+                .surname("Batman")
+                .certs(cert)
+                .build();
+
+        applicantService.deleteApplication(application1, testApplicant);
+
+        Applicant resultApplicant = applicantService.findByUniserial(testApplicant.getUniserial());
+        assertThat(resultApplicant.getApplications()).isEmpty();
+    }
+
 }
