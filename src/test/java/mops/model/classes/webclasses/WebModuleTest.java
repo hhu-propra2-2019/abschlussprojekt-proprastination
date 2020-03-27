e1ed446ccfd0711e517bdb7e3911769ca2c5f7f9
Julia Block <jublo@hhu.de>
2020-03-27 03:10:40 +0100

    Finished WebModuleTest

--- src/test/java/mops/model/classes/webclasses/WebModuleTest.java
+++ src/test/java/mops/model/classes/webclasses/WebModuleTest.java
@@ -1,5 +1,6 @@
 package mops.model.classes.webclasses;
 
+import org.bouncycastle.math.raw.Mod;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import mops.model.classes.Module;
@@ -20,13 +21,14 @@ class WebModuleTest {
                 .nineHourLimit("7")
                 .seventeenHourLimit("1")
                 .build();
+
     }
 
     @Test
     void toStringArray() {
-        String[] output = {"Programmier Praktikum", "ProPra", "Jens", "2", "7", "1", "20"};
-
-        assertThat(webmodule.toStringArray()).isEqualTo(output);
+        String[] output = {"Programmier Praktikum", "ProPra", "Jens", "2", "7", "1"};
+        String[] moduleString = webmodule.toStringArray();
+        assertThat(moduleString).isEqualTo(output);
     }
 
     @Test
@@ -39,7 +41,76 @@ class WebModuleTest {
                 .nineHourLimit("7")
                 .seventeenHourLimit("1")
                 .build();
+        Module m = webmodule.toModule();
+        assertThat(m).isEqualTo(module);
+    }
 
-        assertThat(webmodule.toModule()).isEqualTo(module);
+    @Test
+    void allArgsConstructorTest() {
+        WebModule full = new WebModule("Magische Tränke", "MaTrä",
+                "Maggie Smith", "3",
+                "4", "5");
+
+        assertThat(full.getName()).isEqualTo("Magische Tränke");
+        assertThat(full.getShortName()).isEqualTo("MaTrä");
+        assertThat(full.getProfSerial()).isEqualTo("Maggie Smith");
+        assertThat(full.getSevenHourLimit()).isEqualTo("3");
+        assertThat(full.getNineHourLimit()).isEqualTo("4");
+        assertThat(full.getSeventeenHourLimit()).isEqualTo("5");
+    }
+
+    @Test
+    void noArgsConstructorTest() {
+        WebModule empty = new WebModule();
+
+        assertThat(empty.getName()).isNull();
+        assertThat(empty.getShortName()).isNull();
+        assertThat(empty.getProfSerial()).isNull();
+        assertThat(empty.getSevenHourLimit()).isNull();
+        assertThat(empty.getNineHourLimit()).isNull();
+        assertThat(empty.getSeventeenHourLimit()).isNull();
+    }
+
+    @Test
+    void setNameTest() {
+        webmodule.setName("Zauberei");
+
+        assertThat(webmodule.getName()).isEqualTo("Zauberei");
+    }
+
+    @Test
+    void testEquals() {
+        WebModule webmodule2 = WebModule.builder()
+                .name("Programmier Praktikum")
+                .shortName("ProPra")
+                .profSerial("Jens")
+                .sevenHourLimit("2")
+                .nineHourLimit("7")
+                .seventeenHourLimit("1")
+                .build();
+
+        assertThat(webmodule).isEqualTo(webmodule2);
+    }
+
+    @Test
+    void testHashCode() {
+        WebModule webmodule2 = WebModule.builder()
+                .name("Programmier Praktikum")
+                .shortName("ProPra")
+                .profSerial("Jens")
+                .sevenHourLimit("2")
+                .nineHourLimit("7")
+                .seventeenHourLimit("1")
+                .build();
+
+        assertThat(webmodule.hashCode()).isEqualTo(webmodule2.hashCode());
+    }
+
+    @Test
+    void testToString() {
+        assertThat(webmodule.toString()).isEqualTo("WebModule(name=Programmier Praktikum, "+
+                "shortName=ProPra, profSerial=Jens, " +
+                "sevenHourLimit=2, nineHourLimit=7, " +
+                "seventeenHourLimit=1)");
     }
 }
