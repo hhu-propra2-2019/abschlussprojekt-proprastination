df8076e8e213be9f236db5755d77ab2277cf519a
chber119 <chiara.berry@gmx.de>
2020-03-28 15:10:37 +0100

    test buildCertificate

--- src/test/java/mops/services/webServices/StudentServiceTest.java
+++ src/test/java/mops/services/webServices/StudentServiceTest.java
@@ -1,7 +1,9 @@
 package mops.services.webServices;
 
 import mops.model.classes.Address;
+import mops.model.classes.Certificate;
 import mops.model.classes.webclasses.WebAddress;
+import mops.model.classes.webclasses.WebCertificate;
 import mops.services.dbServices.ApplicantService;
 import mops.services.dbServices.ModuleService;
 import org.junit.jupiter.api.Test;
@@ -32,6 +34,16 @@ class StudentServiceTest {
             .zipcode("77777")
             .build();
 
+    private WebCertificate webCertificate = WebCertificate.builder()
+            .graduation("Master")
+            .graduationcourse("Desaster")
+            .build();
+
+    private Certificate certificate = Certificate.builder()
+            .name("Master")
+            .course("Desaster")
+            .build();
+
     @Test
     void buildAddress() {
         Address result = studentService.buildAddress(webAddress);
@@ -39,6 +51,11 @@ class StudentServiceTest {
         assertEquals(result, address);
     }
 
+    @Test
+    void buildCertificate() {
+        Certificate result =studentService.buildCertificate(webCertificate);
 
+        assertEquals(result, certificate);
+    }
 
 }
