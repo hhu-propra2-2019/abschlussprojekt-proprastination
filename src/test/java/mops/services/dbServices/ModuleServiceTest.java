87709979432d9674ce266d493fb0c0881c06cb97
chber119 <chiara.berry@gmx.de>
2020-03-28 01:20:00 +0100

    test deleteModule in ModuleService

--- src/test/java/mops/services/dbServices/ModuleServiceTest.java
+++ src/test/java/mops/services/dbServices/ModuleServiceTest.java
@@ -8,24 +8,25 @@ import org.junit.jupiter.api.Test;
 import java.time.Instant;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class ModuleServiceTest {
 
     private ModuleRepository repositoryMock = mock(ModuleRepository.class);
     private ModuleService moduleService = new ModuleService(repositoryMock);
 
+    Module module = Module.builder()
+            .name("Kung Fu")
+            .shortName("KF")
+            .profSerial("neo001")
+            .deadline(Instant.parse("1970-01-01T00:00:00Z"))
+            .sevenHourLimit("70")
+            .nineHourLimit("90")
+            .seventeenHourLimit("170")
+            .build();
+
     @Test
     void toWebModule() {
-        Module module = new Module();
-        module.setName("Kung Fu");
-        module.setShortName("KF");
-        module.setProfSerial("neo001");
-        module.setDeadline(Instant.parse("1970-01-01T00:00:00Z"));
-        module.setSevenHourLimit("70");
-        module.setNineHourLimit("90");
-        module.setSeventeenHourLimit("170");
-
         WebModule expected = new WebModule();
         expected.setName("Kung Fu");
         expected.setShortName("KF");
@@ -37,5 +38,14 @@ class ModuleServiceTest {
         WebModule result = moduleService.toWebModule(module);
 
         assertEquals(expected, result);
+    }
+
+    @Test
+    void deleteModule() {
+        when(repositoryMock.findDistinctByName("KungFu")).thenReturn(module);
+
+        moduleService.deleteModule("KungFu");
+
+        verify(repositoryMock, times(1)).deleteById(module.getId());
     }
 }
