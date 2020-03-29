0d599fbe5cf3933e80e76db03522f3e6f6fe6e92
chber119 <chiara.berry@gmx.de>
2020-03-29 15:13:19 +0200

    integrate some of Paulin's test into my own mess

--- src/test/java/mops/services/webServices/StudentServiceTest.java
+++ src/test/java/mops/services/webServices/StudentServiceTest.java
@@ -5,7 +5,6 @@ import mops.model.classes.Applicant;
 import mops.model.classes.Application;
 import mops.model.classes.Certificate;
 import mops.model.classes.Module;
-import mops.model.classes.Certificate;
 import mops.model.classes.webclasses.WebAddress;
 import mops.model.classes.webclasses.WebApplicant;
 import mops.model.classes.webclasses.WebCertificate;
@@ -13,7 +12,7 @@ import mops.services.dbServices.ApplicantService;
 import mops.services.dbServices.ModuleService;
 import org.junit.jupiter.api.Test;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -22,13 +21,8 @@ import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 class StudentServiceTest {
 
@@ -88,6 +82,113 @@ class StudentServiceTest {
             .status("Tutor")
             .checked(false)
             .build();
+
+    private WebCertificate webCertificate1;
+    private WebCertificate webCertificate2;
+
+    @Test
+    void buildAddressTest() {
+        Address address = Address.builder()
+                .street(webAddress.getStreet())
+                .houseNumber(webAddress.getNumber())
+                .city(webAddress.getCity())
+                .zipcode(webAddress.getZipcode())
+                .country(webAddress.getCountry())
+                .build();
+        Address address1 = studentService.buildAddress(webAddress);
+        assertThat(address).isEqualTo(address1);
+    }
+
+    @Test
+    void buildCertificateTest() {
+        webCertificate1 = WebCertificate.builder()
+                .graduation("Bachelor")
+                .graduationcourse("Informatik")
+                .build();
+        webCertificate2 = WebCertificate.builder()
+                .graduationcourse("none")
+                .graduation("")
+                .build();
+        Certificate certificate1 =  Certificate.builder()
+                .name(webCertificate1.getGraduation())
+                .course(webCertificate1.getGraduationcourse())
+                .build();
+        Certificate certificate2 =  Certificate.builder()
+                .name(webCertificate2.getGraduation())
+                .course(webCertificate2.getGraduationcourse())
+                .build();
+        Certificate certificateTest1 = studentService.buildCertificate(webCertificate1);
+        Certificate certificateTest2 = studentService.buildCertificate(webCertificate2);
+        assertThat(certificate1).isEqualTo(certificateTest1);
+        assertThat(certificate2).isEqualTo(certificateTest2);
+    }
+
+    @Test
+    void buildApplicantTest1() {
+        webCertificate1 = WebCertificate.builder()
+                .graduation("Bachelor")
+                .graduationcourse("Informatik")
+                .build();
+        webCertificate2 = WebCertificate.builder()
+                .graduationcourse("none")
+                .graduation("")
+                .build();
+        Address address = studentService.buildAddress(webAddress);
+        Certificate certificate = studentService.buildCertificate(webCertificate1);
+        Applicant applicant = Applicant.builder()
+                .uniserial("mamus100")
+                .firstName("Max")
+                .surname("Mustermann")
+                .address(address)
+                .birthday(webApplicant.getBirthday())
+                .birthplace(webApplicant.getBirthplace())
+                .gender(webApplicant.getGender())
+                .nationality(webApplicant.getNationality())
+                .course(webApplicant.getCourse())
+                .status(webApplicant.getStatus())
+                .certs(certificate)
+                .comment(webApplicant.getComment())
+                .checked(false)
+                .build();
+        Applicant applicantTest = studentService.buildApplicant("mamus100", webApplicant, address, certificate,
+                "Max", "Mustermann");
+        assertThat(applicant).isEqualTo(applicantTest);
+
+    }
+
+    @Test
+    void buildApplicantTest2() {
+        webCertificate1 = WebCertificate.builder()
+                .graduation("Bachelor")
+                .graduationcourse("Informatik")
+                .build();
+        webCertificate2 = WebCertificate.builder()
+                .graduationcourse("none")
+                .graduation("")
+                .build();
+        Address address = studentService.buildAddress(webAddress);
+        Certificate certificate = studentService.buildCertificate(webCertificate1);
+        Applicant applicant = Applicant.builder()
+                .uniserial("mamus100")
+                .firstName("Max")
+                .surname("Mustermann")
+                .address(address)
+                .birthday(webApplicant.getBirthday())
+                .birthplace(webApplicant.getBirthplace())
+                .gender(webApplicant.getGender())
+                .nationality(webApplicant.getNationality())
+                .course(webApplicant.getCourse())
+                .status(webApplicant.getStatus())
+                .certs(certificate)
+                .comment(webApplicant.getComment())
+                .checked(false)
+                .build();
+        // Saving applicant to database.
+        applicantServiceMock.saveApplicant(applicant);
+        Applicant applicantTest = studentService.buildApplicant("mamus100", webApplicant, address, certificate,
+                "Max", "Mustermann");
+        assertThat(applicant).isEqualTo(applicantTest);
+    }
 
     @Test
     void buildAddress() {
@@ -213,13 +314,12 @@ class StudentServiceTest {
         assertEquals(modules, result);
     }
 
-    //Instants werden nicht mehr benutzt laut Nemo
     @Test
     void getAllNotfilledModulesApplicationsGetSortedOut() {
         Module applied = mock(Module.class);
-        when(applied.getDeadline()).thenReturn(Instant.MAX);
+        when(applied.getDeadline()).thenReturn(LocalDateTime.MAX);
         Module notApplied = mock(Module.class);
-        when(notApplied.getDeadline()).thenReturn(Instant.MAX);
+        when(notApplied.getDeadline()).thenReturn(LocalDateTime.MAX);
         List<Module> modules = new ArrayList<>();
         modules.add(applied);
         modules.add(notApplied);
@@ -234,121 +334,5 @@ class StudentServiceTest {
 
         assertThat(result).containsOnly(notApplied);
     }
-
-    //Ab Hier Paulins Sachen
-    WebCertificate webCertificate1;
-    WebCertificate webCertificate2;
-
-
-    @BeforeAll
-    void setup() {
-       webCertificate1 = WebCertificate.builder()
-                .graduation("Bachelor")
-                .graduationcourse("Informatik")
-                .build();
-       webCertificate2 = WebCertificate.builder()
-                .graduationcourse("none")
-                .graduation("")
-                .build();
-   }
-
-    @AfterEach
-    void doAfterEach() {
-        applicantServiceMock.deleteAll();
-    }
-
-    @Test
-    void buildAddressTest() {
-        Address address = Address.builder()
-                .street(webAddress.getStreet())
-                .houseNumber(webAddress.getNumber())
-                .city(webAddress.getCity())
-                .zipcode(webAddress.getZipcode())
-                .country(webAddress.getCountry())
-                .build();
-        Address address1 = studentService.buildAddress(webAddress);
-        assertThat(address).isEqualTo(address1);
-    }
-
-    @Test
-    void buildCertificateTest() {
-        Certificate certificate1 =  Certificate.builder()
-                .name(webCertificate1.getGraduation())
-                .course(webCertificate1.getGraduationcourse())
-                .build();
-        Certificate certificate2 =  Certificate.builder()
-                .name(webCertificate2.getGraduation())
-                .course(webCertificate2.getGraduationcourse())
-                .build();
-        Certificate certificateTest1 = studentService.buildCertificate(webCertificate1);
-        Certificate certificateTest2 = studentService.buildCertificate(webCertificate2);
-        assertThat(certificate1).isEqualTo(certificateTest1);
-        assertThat(certificate2).isEqualTo(certificateTest2);
-    }
-
-    @Test
-    void buildApplicantTest1() {
-        Address address = studentService.buildAddress(webAddress);
-        Certificate certificate = studentService.buildCertificate(webCertificate1);
-        Applicant applicant = Applicant.builder()
-                .uniserial("mamus100")
-                .firstName("Max")
-                .surname("Mustermann")
-                .address(address)
-                .birthday(webApplicant.getBirthday())
-                .birthplace(webApplicant.getBirthplace())
-                .gender(webApplicant.getGender())
-                .nationality(webApplicant.getNationality())
-                .course(webApplicant.getCourse())
-                .status(webApplicant.getStatus())
-                .certs(certificate)
-                .comment(webApplicant.getComment())
-                .checked(false)
-                .build();
-        Applicant applicantTest = studentService.buildApplicant("mamus100", webApplicant, address, certificate,
-                "Max", "Mustermann");
-        assertThat(applicant).isEqualTo(applicantTest);
-
-    }
-
-    @Test
-    void buildApplicantTets2() {
-        Address address = studentService.buildAddress(webAddress);
-        Certificate certificate = studentService.buildCertificate(webCertificate1);
-        Applicant applicant = Applicant.builder()
-                .uniserial("mamus100")
-                .firstName("Max")
-                .surname("Mustermann")
-                .address(address)
-                .birthday(webApplicant.getBirthday())
-                .birthplace(webApplicant.getBirthplace())
-                .gender(webApplicant.getGender())
-                .nationality(webApplicant.getNationality())
-                .course(webApplicant.getCourse())
-                .status(webApplicant.getStatus())
-                .certs(certificate)
-                .comment(webApplicant.getComment())
-                .checked(false)
-                .build();
-        applicantServiceMock.saveApplicant(applicant);
-        Applicant applicantTest = studentService.buildApplicant("mamus100", webApplicant, address, certificate,
-                "Max", "Mustermann");
-        assertThat(applicant).isEqualTo(applicantTest);
-    }
-
-/*    @Test
-    void updateApplicantTest() {
-        Address address = studentService.buildAddress(webAddress);
-        Certificate certificate1 = studentService.buildCertificate(webCertificate1);
-        Certificate certificate2 = studentService.buildCertificate(webCertificate2);
-        Applicant applicant1 = studentService.buildApplicant("mamus100", webApplicant, address, certificate1,
-                "Max", "Mustermann");
-        applicantService.saveApplicant(applicant1);
-        Applicant applicant2 = studentService.buildApplicant("mamus100", webApplicant, address, certificate2,
-        "Max", "Mustermann");
-        studentService.updateApplicantWithoutChangingApplications(applicant2);
-        Applicant applicantTest = applicantService.findByUniserial("mamus100");
-        assertThat(applicant2).isEqualTo(applicantTest);
-    }*/
 
 }
